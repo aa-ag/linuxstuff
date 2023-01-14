da934f8b73764e54001c36cb654debc79a87e3fe
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-13 20:04:37 -0600

    Add loop to find, copy and add headers from first partition into the rest of the split files

--- scripts.sh
+++ scripts.sh
@@ -2,3 +2,7 @@
 split -l 1000 -d fake_data.csv prefix_
 
 for i in $(find prefix_*); do mv $i "$i.csv"; done
+
+for i in $(find . -type f -name "prefix_*.csv" -not -name "prefx_00.csv");
+    do echo -e "$(head -l prefix_00.csv)\n(cat $i)" > $i;
+done
