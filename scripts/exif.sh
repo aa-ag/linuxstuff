253141ab61be38408a668369fe7b3a5aa5120c32
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-14 13:12:45 -0600

    Add >> to store result in a file for ease of transmission and readability

--- scripts/exif.sh
+++ scripts/exif.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # https://exiftool.org/
 brew install exiftool
-exiftool test_data/joshua-j-cotten-Bxn2VB0O1sU-unsplash.jpg
+exiftool test_data/joshua-j-cotten-Bxn2VB0O1sU-unsplash.jpg >> 'test_data/exif.txt'
