7fe4d2254d594bab30085d8a71728c9b9ad4d325
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-15 18:08:15 -0600

    Add version of one file in resulting zip folder

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 zip -r test.zip test_data
+
+zip test.zip test_data/joshua-j-cotten-Bxn2VB0O1sU-unsplash.jpg
+
+# https://imagemagick.org/
+convert <input file> quality 10% <output file>
