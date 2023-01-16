09bbce149872d8be01bdd8b13755c737792d6c22
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-16 09:04:19 -0600

    Add graphicsmagick install

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -5,3 +5,6 @@ zip test.zip test_data/joshua-j-cotten-Bxn2VB0O1sU-unsplash.jpg
 
 # https://imagemagick.org/
 convert <input file> quality 10% <output file>
+
+# http://www.graphicsmagick.org/
+brew install graphicsmagick
