b4df2730633642eb9c5b2edb809543d23b96e42a
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-17 22:02:52 -0600

    Add resizing command

--- scripts/gm.sh
+++ scripts/gm.sh
@@ -4,3 +4,6 @@
 
 ## format
 gm convert test_data/joshua-j-cotten-Bxn2VB0O1sU-unsplash.jpg test_photo.png
+
+## thumbnail
+gm convert -size 120x120 test_data/joshua-j-cotten-Bxn2VB0O1sU-unsplash.jpg -resize 120x120 +profile "*" thumbnail.jpg
