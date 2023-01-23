c4b31ec92fcee8ade868e7b1877a4748dbe8715d
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-22 21:26:02 -0600

    Repalce tab separators for commans for headers

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -8,4 +8,4 @@ cat test_data/text_test_file.txt | tr -s '[:blank:]' ',' > test_data/text_conver
 cat test_data/text_converted_to_csv_test_file.csv | tr -s ',' ' ' > test_data/csv_converted_back_to_text_test_file.txt
 
 ## add header
-echo -e "1\t2\t3" | cat - test_data/text_converted_to_csv_test_file.csv > test_data/text_converted_to_csv_with_headers_test_file.csv
+echo -e "1,2,3" | cat - test_data/text_converted_to_csv_test_file.csv > test_data/text_converted_to_csv_with_headers_test_file.csv
