920743c37086e33c56170f50d4e56b2e641be496
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-22 21:21:41 -0600

    Comment for documentation

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -4,4 +4,5 @@ sed 's/ \+/,/g' test_data/text_test_file.txt > test_data/text_converted_to_csv_t
 
 cat test_data/text_test_file.txt | tr -s '[:blank:]' ',' > test_data/text_converted_to_csv_test_file.csv
 
+## .csv to .txt
 cat test_data/text_converted_to_csv_test_file.csv | tr -s ',' ' ' > test_data/csv_converted_back_to_text_test_file.txt
