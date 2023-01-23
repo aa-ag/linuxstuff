e863cfdf9779371fccbb7ee655dc5cbac1b7a19b
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-22 21:08:54 -0600

    Convert csv back to .txt with white spaces replacing comma separators

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -3,3 +3,5 @@
 sed 's/ \+/,/g' test_data/text_test_file.txt > test_data/text_converted_to_csv_test_file.csv
 
 cat test_data/text_test_file.txt | tr -s '[:blank:]' ',' > test_data/text_converted_to_csv_test_file.csv
+
+cat test_data/text_converted_to_csv_test_file.csv | tr -s ',' ' ' > test_data/csv_converted_back_to_text_test_file.txt
