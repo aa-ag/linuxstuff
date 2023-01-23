427e784ad07bc51185a2ca713754bc1072ed629d
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-22 21:24:37 -0600

    Adding headers logic

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -6,3 +6,6 @@ cat test_data/text_test_file.txt | tr -s '[:blank:]' ',' > test_data/text_conver
 
 ## .csv to .txt
 cat test_data/text_converted_to_csv_test_file.csv | tr -s ',' ' ' > test_data/csv_converted_back_to_text_test_file.txt
+
+## add header
+echo -e "1\t2\t3" | cat - test_data/text_converted_to_csv_test_file.csv > test_data/text_converted_to_csv_with_headers_test_file.csv
