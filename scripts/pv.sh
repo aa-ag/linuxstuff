27fd46422b2e89754a6b23977352a7aa65550846
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-28 21:21:21 -0600

    Add three other options

--- scripts/pv.sh
+++ scripts/pv.sh
@@ -7,3 +7,11 @@ $ pv -p test_data/fake_data.csv | zip > test_data/fakeDataZip.zip
 
 ### time
 $ pv -t test_data/fake_data.csv | zip > test_data/fakeDataZip.zip
+
+### eta
+$ pv -e test_data/fake_data.csv | zip > test_data/fakeDataZip.zip
+
+### other
+* -r # rate
+* -b # byte
+* -n # int/%
