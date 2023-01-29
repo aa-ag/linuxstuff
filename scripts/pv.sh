80c073f263b7513f757e26c31caf493f17962a3e
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-28 21:18:44 -0600

    Add -p for time view

--- scripts/pv.sh
+++ scripts/pv.sh
@@ -2,4 +2,8 @@
 
 $ brew install pv
 
+### percentage
 $ pv -p test_data/fake_data.csv | zip > test_data/fakeDataZip.zip
+
+### time
+$ pv -t test_data/fake_data.csv | zip > test_data/fakeDataZip.zip
