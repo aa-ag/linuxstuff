43992d5cbcc91bd43bf659e8009cf39faad15794
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-31 20:26:08 -0600

    Refactor to make ecrypt and decrypt executable

---
+++ scripts/decrypt.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+## decode
+echo $1 | openssl aes-256-cbc -d -a -pass pass:thïs.¡s,@\;t3st
--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-$ openssl version
-
-$ echo $1 | openssl aes-256-cbc -a -salt -pass pass:thïs.¡s,@;t3st
+# $ openssl version
+## encode
+echo $1 | openssl aes-256-cbc -a -salt -pass pass:thïs.¡s,@\;t3st
