9108387e4a62bb3af91e89aa60b7f0ad2f29527f
aa-ag <aaron.aguerrevere@gmail.com>
2023-02-01 20:45:37 -0600

    Add decrytion of file with password

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ## decode
-echo $1 | openssl aes-256-cbc -d -a -pass pass:thïs.¡s,@\;t3st
+# echo $1 | openssl aes-256-cbc -d -a -pass pass:thïs.¡s,@\;t3st
+openssl enc -in test.enc -d -aes-256-cbc -pass stdin > test.txt
