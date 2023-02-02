030998b307c1b7c355ba07de5f51a871034b0f87
aa-ag <aaron.aguerrevere@gmail.com>
2023-02-01 20:32:55 -0600

    Add version to encrypt while files

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # $ openssl version
 ## encode
-echo $1 | openssl aes-256-cbc -a -salt -pass pass:thïs.¡s,@\;t3st
+# echo $1 | openssl aes-256-cbc -a -salt -pass pass:thïs.¡s,@\;t3st
+openssl enc -in test_data/text_test_file.txt -aes-256-cbc -pass stdin > text_test_file.text.enc
