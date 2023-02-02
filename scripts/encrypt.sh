76c80f13d66889de1ba600452a42d2296025fafd
aa-ag <aaron.aguerrevere@gmail.com>
2023-02-01 20:45:14 -0600

    Attempt to pass password in command

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -2,4 +2,4 @@
 # $ openssl version
 ## encode
 # echo $1 | openssl aes-256-cbc -a -salt -pass pass:thïs.¡s,@\;t3st
-openssl enc -in test_data/text_test_file.txt -aes-256-cbc -pass stdin > text_test_file.text.enc
+openssl enc -in test_data/text_test_file.txt -aes-256-cbc -d -a -pass pass:pswrd > test.enc
