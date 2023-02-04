d648b4b903e0326acca7e40234c09a2718e5285c
aa-ag <aaron.aguerrevere@gmail.com>
2023-02-03 20:20:36 -0600

    Add command to generate a key using a specific algo

--- scripts/openssl.sh
+++ scripts/openssl.sh
@@ -3,3 +3,8 @@
 $ openssl version -a
 
 $ wget https://www.openssl.org/source/openssl-1.1.1g.tar.gz
+
+# Privacy-Enhanced Mail (PEM) format
+$ http://curl.haxx.se/docs/caextract.html
+
+$ openssl genpkey -out fd.key -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -aes-128-cbc
