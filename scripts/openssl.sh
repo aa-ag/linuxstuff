b0a81b596be499b7eb20a2361acad0cce54c8e2f
aa-ag <aaron.aguerrevere@gmail.com>
2023-02-04 19:18:24 -0600

    Add looking public part of the key command

--- scripts/openssl.sh
+++ scripts/openssl.sh
@@ -8,3 +8,9 @@ $ wget https://www.openssl.org/source/openssl-1.1.1g.tar.gz
 $ http://curl.haxx.se/docs/caextract.html
 
 $ openssl genpkey -out fd.key -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -aes-128-cbc
+
+$ openssl pkey -in fd.key -text -noout
+
+$ openssl pkey -in fd.key -pubout -out fd-public.key
+
+$ cat fd-public.key
