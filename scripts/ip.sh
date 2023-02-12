5104f418f13fed9f1a316262dce11796b2a37a0e
aa-ag <aaron.aguerrevere@gmail.com>
2023-02-12 13:59:23 -0600

    Add version from stackoverflow mentioned

--- scripts/ip.sh
+++ scripts/ip.sh
@@ -5,3 +5,7 @@ hostname
 /sbin/ifconfig
 
 host myip.opendns.com resolver1.opendns.com
+
+## MacOS: https://apple.stackexchange.com/questions/20547/how-do-i-find-my-ip-address-from-the-command-line
+#### wireless:
+ipconfig getifaddr en1
