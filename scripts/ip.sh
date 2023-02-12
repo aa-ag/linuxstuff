8c173b92a74cb0e2c7200c2778e7a3356db91e67
aa-ag <aaron.aguerrevere@gmail.com>
2023-02-12 14:02:54 -0600

    Add curl version

--- scripts/ip.sh
+++ scripts/ip.sh
@@ -2,6 +2,7 @@
 
 hostname
 
+## list of interfaces with their IP and MAC Addressess
 /sbin/ifconfig
 
 host myip.opendns.com resolver1.opendns.com
@@ -9,3 +10,6 @@ host myip.opendns.com resolver1.opendns.com
 ## MacOS: https://apple.stackexchange.com/questions/20547/how-do-i-find-my-ip-address-from-the-command-line
 #### wireless:
 ipconfig getifaddr en1
+
+## easiest to get public address from 
+curl ifconfig.me
