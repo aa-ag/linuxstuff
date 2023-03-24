b64f01a515a618d6be9c368716e52f45f6d13f34
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-24 11:50:19 -0500

    Clean commit message string

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-read -p "Commit message: " cm
+read -p "commit message: " cm
 THISFILE="$(pwd)/${BASH_SOURCE[0]}"
 git add $THISFILE
-git commit -m "{$cm}"
+git commit -m "$cm"
 git push
