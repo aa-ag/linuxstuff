111765ca75d358f0106cced88f4c82b72c28a524
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-18 18:00:53 -0500

    Add command to have netcat 'use' demo.dock as unix socket file and 'listen':

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-SNAPSHOT=1
-while :
-do  
-    top -l 1 > "../output/top_output_$SNAPSHOT.txt"
-    sleep 60
-    ((SNAPSHOT++))
-done
+# SNAPSHOT=1
+# while :
+# do  
+#     top -l 1 > "../output/top_output_$SNAPSHOT.txt"
+#     sleep 60
+#     ((SNAPSHOT++))
+# done
+nc -U /tmp/demo.sock -l
