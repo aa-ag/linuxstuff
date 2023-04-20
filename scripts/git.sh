c691e30190f49f90ba83444d0c97d18ff7b631ff
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-19 19:47:13 -0500

    Add echo of remote for better UX

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+REMOTE=$(git remote -v)
+arr=( $REMOTE )
 read -p "git add: " addme
 git add "$addme"
 read -p "commit message: " cm
 git commit -m "$cm"
+echo '\n ^ pushing to' "${arr[4]}\n"
 git push
