770f310b5dd0c20577d84437e204096ee0025754
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-20 12:22:37 -0500

    Add global snippet config for VSC

--- scripts/gitme.sh
+++ scripts/gitme.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+: ' {
+	"gitme": {
+		"prefix": "gitme",
+		"body": [
+			"#!/bin/bash",
+			"REMOTE=$(git remote -v)",
+			"arr=( $REMOTE )",
+			"read -p \"git add: \" addme",
+			"git add \"$$addme\"",
+			"read -p \"commit message: \" cm",
+			"git commit -m \"$$cm\" --quiet",
+			"echo '\\n ^ pushing to' \"${arr[4]}\\n\"",
+			"git push --quiet",
+		],
+		"description": "Bash script to prompt git add, commit & push"
+	}
+}
+'
+
 REMOTE=$(git remote -v)
 arr=( $REMOTE )
 read -p "git add: " addme
