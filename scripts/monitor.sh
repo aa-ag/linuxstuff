4a578d7ce93b6075f8b2ce5eb347c7ee4eb3b715
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-20 14:52:40 -0500

    Testing email logic

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-# SNAPSHOT=1
-# while :
-# do  
-#     top -l 1 > "../output/top_output_$SNAPSHOT.txt"
-#     sleep 60
-#     ((SNAPSHOT++))
-# done
-nc -U /tmp/demo.sock -l
-lsof /tmp/demo.sock
-nc -l 1234
-nc -N 127.0.0.1 1234
+SNAPSHOT=1
+while :
+do  
+    top -l 1 > "../output/top_output_$SNAPSHOT.txt"
+    sendmail aaron@aguerrevere.dev < "../output/top_output_$SNAPSHOT.txt"
+    rm "../output/top_output_$SNAPSHOT.txt"
+    sleep 60
+    ((SNAPSHOT++))
+done
+# nc -U /tmp/demo.sock -l
+# lsof /tmp/demo.sock
+# nc -l 1234
+# nc -N 127.0.0.1 1234
