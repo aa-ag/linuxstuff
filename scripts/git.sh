916737e08a19294690ec20404f31880dfd4fec4d
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-19 19:53:29 -0500

    Make commit also quiet

--- scripts/git.sh
+++ scripts/git.sh
@@ -4,6 +4,6 @@ arr=( $REMOTE )
 read -p "git add: " addme
 git add "$addme"
 read -p "commit message: " cm
-git commit -m "$cm"
+git commit -m "$cm" --quiet
 echo '\n ^ pushing to' "${arr[4]}\n"
 git push --quiet
