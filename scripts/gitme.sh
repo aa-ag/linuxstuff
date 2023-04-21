744823ec8bd70348463b9a728c5c15d40cd1aebe
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-21 14:51:24 -0500

    Add loop to ask for user input until file exists"
    Signed-off-by: aa-ag <aaron.aguerrevere@gmail.com>

--- scripts/gitme.sh
+++ scripts/gitme.sh
@@ -21,14 +21,15 @@
 # echo $THISFILE
 REMOTE=$(git remote -v)
 arr=( $REMOTE )
-read -p "git add: " addme
-if [[ -f "$addme" ]]; then
-    git add "$addme"
-    read -p "commit message: " cm
-    git commit -s -m "$cm" --quiet
-    echo '\n ^ pushing to' "${arr[4]}\n"
-    git push --quiet
-else
-    echo "File does not exist. Start over."
-    exit 1
-fi
+addme=''
+while [ ! -f "$addme" ]
+do  
+    read -p "git add: " addme
+    echo 'File does not exist: try again.'
+done
+echo '\ngreat! added "$addme" to stagin'
+git add "$addme"
+read -p "commit message: " cm
+git commit -s -m "$cm" --quiet
+echo '\n ^ pushing to' "${arr[4]}\n"
+git push --quiet
