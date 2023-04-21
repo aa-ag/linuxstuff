bbe492fec2faeca79381a363d81e86fdf5aec625
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-21 14:53:58 -0500

    Include while loop version of gitme
    Signed-off-by: aa-ag <aaron.aguerrevere@gmail.com>

--- scripts/generate_new_project.sh
+++ scripts/generate_new_project.sh
@@ -20,10 +20,16 @@ touch gitme.sh
 cat >gitme.sh <<'EOL'
 REMOTE=$(git remote -v)
 arr=( $REMOTE )
-read -p "git add: " addme
+addme=''
+while [ ! -f "$addme" ]
+do  
+    read -p "git add: " addme
+    echo 'File does not exist: try again.'
+done
+echo "\ngreat! added $addme to stagin"
 git add "$addme"
 read -p "commit message: " cm
-git commit -m "$cm" --quiet
+git commit -s -m "$cm" --quiet
 echo '\n ^ pushing to' "${arr[4]}\n"
 git push --quiet
 EOL
