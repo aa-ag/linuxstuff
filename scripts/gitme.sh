f15a7bb8ea7fbbd685f0a9154ef60bfb4903d080
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-21 14:40:40 -0500

    Check if file inputed by user exists

--- scripts/gitme.sh
+++ scripts/gitme.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
-: ' {
-	"gitme": {
-		"prefix": "gitme",
-		"body": [
-			"#!/bin/bash",
-			"REMOTE=$(git remote -v)",
-			"arr=( $$REMOTE )",
-			"read -p \"git add: \" addme",
-			"git add \"$$addme\"",
-			"read -p \"commit message: \" cm",
-			"git commit -m \"$$cm\" --quiet",
-			"echo '\\n ^ pushing to' \"${arr[4]}\\n\"",
-			"git push --quiet",
-		],
-		"description": "Bash script to prompt git add, commit & push"
-	}
-}
-'
-
+# : ' {
+# 	"gitme": {
+# 		"prefix": "gitme",
+# 		"body": [
+# 			"#!/bin/bash",
+# 			"REMOTE=$(git remote -v)",
+# 			"arr=( $$REMOTE )",
+# 			"read -p \"git add: \" addme",
+# 			"git add \"$$addme\"",
+# 			"read -p \"commit message: \" cm",
+# 			"git commit -m \"$$cm\" --quiet",
+# 			"echo '\\n ^ pushing to' \"${arr[4]}\\n\"",
+# 			"git push --quiet",
+# 		],
+# 		"description": "Bash script to prompt git add, commit & push"
+# 	}
+# }
+# '
+# THISFILE="$(pwd)/${BASH_SOURCE[0]}"
+# echo $THISFILE
 REMOTE=$(git remote -v)
 arr=( $REMOTE )
 read -p "git add: " addme
-git add "$addme"
-read -p "commit message: " cm
-git commit -m "$cm" --quiet
-echo '\n ^ pushing to' "${arr[4]}\n"
-git push --quiet
+if [[ -f "$addme" ]]; then
+    git add "$addme"
+    read -p "commit message: " cm
+    git commit -m "$cm" --quiet
+    echo '\n ^ pushing to' "${arr[4]}\n"
+    git push --quiet
+else
+    echo "File does not exist. Start over."
+    exit 1
+fi
