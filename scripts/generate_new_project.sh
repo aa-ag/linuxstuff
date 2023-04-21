e5d0b69e25ff6541ee3f1c4d47bd9a11d62455e9
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-21 10:00:04 -0500

    Add gitme.sh by default

--- scripts/generate_new_project.sh
+++ scripts/generate_new_project.sh
@@ -3,5 +3,15 @@ mkdir "$projectname"
 cd "$projectname"
 touch .gitignore
 touch README.md
-echo "###$projectname\n\n" > README.md
+echo "### $projectname\n\n" > README.md
 touch gitme.sh
+cat >gitme.sh <<EOL
+REMOTE=$(git remote -v)
+arr=( $REMOTE )
+read -p "git add: " addme
+git add "$addme"
+read -p "commit message: " cm
+git commit -m "$cm" --quiet
+echo '\n ^ pushing to' "${arr[4]}\n"
+git push --quiet
+EOL
