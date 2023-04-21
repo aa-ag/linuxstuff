3bfe9b7206498e72360c0f5b02100bb21bf8b674
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-21 10:10:20 -0500

    Add comments & instruction to open VSC

--- scripts/generate_new_project.sh
+++ scripts/generate_new_project.sh
@@ -1,11 +1,17 @@
+# (1) Ask user for project name 
+# & create a new directory
 read -p "Project name: " projectname
 mkdir "$projectname"
 cd "$projectname"
+# (2) create a gitignore file
 touch .gitignore
+echo '.gitignore' gitme.sh
+# (3) create a README
 touch README.md
-echo "### $projectname\n\n" > README.md
+echo "### $projectname\n" > README.md
+# (4) create a gitme 
 touch gitme.sh
-cat >gitme.sh <<EOL
+cat >gitme.sh <<'EOL'
 REMOTE=$(git remote -v)
 arr=( $REMOTE )
 read -p "git add: " addme
@@ -15,3 +21,5 @@ git commit -m "$cm" --quiet
 echo '\n ^ pushing to' "${arr[4]}\n"
 git push --quiet
 EOL
+# (5) open the directory
+code .
