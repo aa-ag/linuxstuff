cb98f53fa697488e88ba5496ccb1595f53b8616b
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-21 15:03:36 -0500

    Make prompts more readable
    Signed-off-by: aa-ag <aaron.aguerrevere@gmail.com>

--- scripts/gitme.sh
+++ scripts/gitme.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-# : ' {
-# 	"gitme": {
-# 		"prefix": "gitme",
-# 		"body": [
-# 			"#!/bin/bash",
-# 			"REMOTE=$(git remote -v)",
-# 			"arr=( $$REMOTE )",
-# 			"read -p \"git add: \" addme",
-# 			"git add \"$$addme\"",
-# 			"read -p \"commit message: \" cm",
-# 			"git commit -m \"$$cm\" --quiet",
-# 			"echo '\\n ^ pushing to' \"${arr[4]}\\n\"",
-# 			"git push --quiet",
-# 		],
-# 		"description": "Bash script to prompt git add, commit & push"
-# 	}
-# }
-# '
 # THISFILE="$(pwd)/${BASH_SOURCE[0]}"
 # echo $THISFILE
 REMOTE=$(git remote -v)
@@ -24,12 +6,15 @@ arr=( $REMOTE )
 addme=''
 while [ ! -f "$addme" ]
 do  
-    read -p "git add: " addme
-    echo 'File does not exist: try again.'
+    read -p "Path to file you want to add: " addme
+    if [ ! -f "$addme" ]; then
+        echo "\n 🛑 \""$addme"\" doesn't exist.\n"
+    else
+        echo "\n 🟢 Added \"$addme\" to stagin.\n"
+    fi
 done
-echo "\ngreat! added $addme to stagin"
 git add "$addme"
 read -p "commit message: " cm
 git commit -s -m "$cm" --quiet
-echo '\n ^ pushing to' "${arr[4]}\n"
+echo '\n 🛫 pushing to' "${arr[4]} \n"
 git push --quiet
