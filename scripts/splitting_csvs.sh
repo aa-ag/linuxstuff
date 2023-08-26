7476f6cf7279829adc8f134eae2d4f6dae78f1c0
aa-ag <aaron.aguerrevere@gmail.com>
2023-08-25 22:12:50 -0500

    Add first logic to count lines/rows in the file

--- scripts/splitting_csvs.sh
+++ scripts/splitting_csvs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-split -l 1000 -d fake_data.csv prefix_
+LINES=$(cat ../test_data/fake_data.csv | wc -l)
+echo 'fake_data.csv has' $LINES 'lines.'
+# split -l 1000 -d fake_data.csv prefix_
 
-for i in $(find prefix_*); do mv $i "$i.csv"; done
+# for i in $(find prefix_*); do mv $i "$i.csv"; done
 
-for i in $(find . -type f -name "prefix_*.csv" -not -name "prefx_00.csv");
-    do echo -e "$(head -l prefix_00.csv)\n(cat $i)" > $i;
-done
+# for i in $(find . -type f -name "prefix_*.csv" -not -name "prefx_00.csv");
+#     do echo -e "$(head -l prefix_00.csv)\n(cat $i)" > $i;
+# done
