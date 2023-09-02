1527073769a0d74b031dd11932889bcb6ed6de5a
aa-ag <aaron.aguerrevere@gmail.com>
2023-09-02 09:33:08 -0500

    Reduce loop into one sed where head is extracted at the same time

--- scripts/splitting_csvs.sh
+++ scripts/splitting_csvs.sh
@@ -1,9 +1,3 @@
 # !/bin/bash
 split --numeric-suffixes=1 -l 5000 ../test_data/fake_data.csv part_
-
-headers=$(head -n 1 ../test_data/fake_data.csv)
-
-sed -i '1s/^'$headers'\n/' part_*.csv
-# for i in $(find . -type f -name "part_*.csv" -not -name "part_01.csv");
-#     do sed -i '1s/^'$headers'\n/';
-# done
+sed -i '1s/^'`head -n 1 ../test_data/fake_data.csv`'\n/' part_*.csv
