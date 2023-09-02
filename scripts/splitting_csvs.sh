70cf05db85f8342b8f5054c8c4ea4a135ff967c4
aa-ag <aaron.aguerrevere@gmail.com>
2023-09-01 22:09:12 -0500

    Add logic to add headers to splits

--- scripts/splitting_csvs.sh
+++ scripts/splitting_csvs.sh
@@ -1,8 +1,8 @@
 # !/bin/bash
-sudo split --numeric-suffixes=1 -l 5000 ../test_data/fake_data.csv part_
+split --numeric-suffixes=1 -l 5000 ../test_data/fake_data.csv part_
 
 headers=$(head -n 1 ../test_data/fake_data.csv)
 
 for i in $(find . -type f -name "part_*.csv" -not -name "part_01.csv");
-    do sed -i ''$headers'';
+    do sed -i '1s/^'$headers'\n/';
 done
