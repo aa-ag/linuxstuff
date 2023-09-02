6505080389f252dc1666eb6155d47e5409bf5509
aa-ag <aaron.aguerrevere@gmail.com>
2023-09-01 22:01:59 -0500

    Add loop to go thru files to add headers

--- scripts/splitting_csvs.sh
+++ scripts/splitting_csvs.sh
@@ -1,4 +1,8 @@
 # !/bin/bash
-split -l 5000 ../test_data/fake_data.csv part_ --additional-suffix=.csv --numeric-suffixes=1
+sudo split --numeric-suffixes=1 -l 5000 ../test_data/fake_data.csv part_
 
-# head -n 1 ../test_data/fake_data.csv
+headers=$(head -n 1 ../test_data/fake_data.csv)
+
+for i in $(find . -type f -name "part_*.csv" -not -name "part_01.csv");
+    do sed -i ''$headers'';
+done
