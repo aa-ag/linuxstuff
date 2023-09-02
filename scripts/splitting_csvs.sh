a81f4d020054524065e44fb4db729366f719a210
aa-ag <aaron.aguerrevere@gmail.com>
2023-09-02 09:33:45 -0500

    Add && to execute one command without looping or separating steps

--- scripts/splitting_csvs.sh
+++ scripts/splitting_csvs.sh
@@ -1,3 +1,2 @@
 # !/bin/bash
-split --numeric-suffixes=1 -l 5000 ../test_data/fake_data.csv part_
-sed -i '1s/^'`head -n 1 ../test_data/fake_data.csv`'\n/' part_*.csv
+split --numeric-suffixes=1 -l 5000 ../test_data/fake_data.csv part_ && sed -i '1s/^'`head -n 1 ../test_data/fake_data.csv`'\n/' part_*.csv
