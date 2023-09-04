d640ee56a59df7a78ec1f5c14b88a3dc13213df0
aa-ag <aaron.aguerrevere@gmail.com>
2023-09-03 19:07:16 -0500

    Add variables into logic to run a split using them from dev input

--- scripts/splitting_csvs.sh
+++ scripts/splitting_csvs.sh
@@ -1,4 +1,13 @@
 # !/bin/bash
+read -p "Enter the name of the object you are trying to split (a, b, c, etc...): " object
 read -p "Enter the path to the file you want split: " original_csv
-echo $original_csv
-# split --numeric-suffixes=1 -l 5000 ../test_data/fake_data.csv part_ && sed -i '1s/^'`head -n 1 ../test_data/fake_data.csv`'\n/' part_*.csv
+read -p "Enter the path to the destination directory where you want the resulting split stored into: " destination_directory
+read -p "Enter number of rows you want the resulting subfiles to have: " resulting_row_count
+
+split --numeric-suffixes=1 -l $resulting_row_count $original_csv $object'_' 
+
+# headers=$(head -n 1 ../test_data/fake_data.csv)
+
+# for i in $(find . -type f -name "part_*.csv" -not -name "part_01.csv");
+#     do sed -i '1s/^'$headers'\n/';
+# done
