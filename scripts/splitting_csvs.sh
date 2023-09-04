a6c4723c8fdb81c3db7c1405bef551af1b1dc45f
aa-ag <aaron.aguerrevere@gmail.com>
2023-09-03 19:00:23 -0500

    Add feature to store inputs/outputs info on variables to generalize utility

--- scripts/splitting_csvs.sh
+++ scripts/splitting_csvs.sh
@@ -1,2 +1,4 @@
 # !/bin/bash
-split --numeric-suffixes=1 -l 5000 ../test_data/fake_data.csv part_ && sed -i '1s/^'`head -n 1 ../test_data/fake_data.csv`'\n/' part_*.csv
+read -p "Enter the path to the file you want split: " original_csv
+echo $original_csv
+# split --numeric-suffixes=1 -l 5000 ../test_data/fake_data.csv part_ && sed -i '1s/^'`head -n 1 ../test_data/fake_data.csv`'\n/' part_*.csv
